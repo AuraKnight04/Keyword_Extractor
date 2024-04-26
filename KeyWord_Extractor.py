54096feb664cac5649e46b08bd1a5670b26face8
AuraKnight04 <lrafaelvazquez@gmail.com>
2024-04-26 14:03:13 -0400

    Updates to code

--- KeyWord_Extractor.py
+++ KeyWord_Extractor.py
@@ -1,5 +1,6 @@
 import nltk 
 from nltk.corpus import stopwords
+from rake_nltk import Rake
 from textblob import TextBlob
 import requests
 from bs4 import BeautifulSoup
@@ -7,6 +8,9 @@ nltk.download('stopwords')
 
 # initialize the stopwords
 stops = stopwords.words('english')
+
+# Initialize RAKE while excluding repeated phrases
+rake = Rake(include_repeated_phrases=False)
 
 # Get user input for the URL, then send request to that URL to access the text
 # Use Beatiful Soup to scrape the text and store it in a variable. 
@@ -24,12 +28,18 @@ for word in soup_blob.words:
     if word in stops:
          soup_blob.words.remove(word)
 
+rake.extract_keywords_from_text(soup_blob)
+
+keywords = r.get_ranked_phrases
+
+
+
 # This asks the user for the name of the file they want use
 # It makes that file and writes each keyword in soup_blob to it
 # This can then be accessed by the user.
 file_name = input("Enter the desired name for the file; have .txt at the end too:")
 key_file = open(file_name, 'w')
 nl = "\n"
-for word in soup_blob.words:
+for word in keywords:
     key_file.write(word)
     key_file.write(nl)
